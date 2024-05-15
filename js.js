71be2ee68acfd1b91f7b7562ab658985b0ae460f
Bolbotunov <100226065+Bolbotunov@users.noreply.github.com>
2024-05-15 15:39:16 +0300

    Create js.js

---
+++ js.js
@@ -0,0 +1,33 @@
+let cells = [...document.querySelectorAll('.cell')]
+let btn = document.querySelector('.btns_wrapper')
+let btnText = document.querySelector('.btns')
+
+
+// ===============рандомный номер от 1-5===========
+function randomize(min,max){
+  let randomCell =Math.floor(Math.random() * (max - min + 1)) + min;
+  return randomCell
+}
+let a = randomize(1,5)
+//   console.log(a)
+
+  
+cells.forEach((item)=>{
+  item.addEventListener('click',function(){
+    let selectCell = +item.innerHTML
+if(a === selectCell){
+  item.classList.add('win_cell')
+} else{
+  item.classList.add('lose_cell')
+}
+  })
+})
+
+
+function press(){
+  btnText.textContent = btnText.textContent === 'new game' ? 'select cell' : 'new game';
+  cells.forEach((item)=>{
+    item.classList.toggle('change_color')
+  }) 
+}
+btn.addEventListener('click',press)
